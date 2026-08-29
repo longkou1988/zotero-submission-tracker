51f846807bd95cc7e8707c526884bd6e41c99ba5
kouzi <longkou1988@users.noreply.github.com>
2026-08-30 07:58:06 +0800

    chore: fix empty catch lint in bootstrap boot log

--- addon/bootstrap.js
+++ addon/bootstrap.js
@@ -12,7 +12,9 @@ function install(data, reason) {}
 async function startup({ id, version, resourceURI, rootURI }, reason) {
   try {
     Zotero.STBootLog = (Zotero.STBootLog || "") + rootURI + "\n";
-  } catch (e) {}
+  } catch (e) {
+    // boot log is best-effort diagnostics only
+  }
   var aomStartup = Components.classes[
     "@mozilla.org/addons/addon-manager-startup;1"
   ].getService(Components.interfaces.amIAddonManagerStartup);
