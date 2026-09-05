7b77f6d209a890cab428b0baff76f158ea99d5ed
kouzi <150926569+longkou1988@users.noreply.github.com>
2026-09-05 15:49:41 +0800

    feat: expose Springer login in development API

--- src/addon.ts
+++ src/addon.ts
@@ -7,7 +7,10 @@ import { openDashboard, closeDashboard } from "./modules/dashboard";
 import { openCreateDialog } from "./modules/dialog";
 import { checkFollowUps } from "./modules/notify";
 import { runSpringerProbe } from "./modules/statusSync/springerProbe";
-import { runSpringerDiscoveryCheck } from "./modules/statusSync/springerDiscoveryCheck";
+import {
+  openSpringerLogin,
+  runSpringerDiscoveryCheck,
+} from "./modules/statusSync/springerDiscoveryCheck";
 
 class Addon {
   public data: {
@@ -33,6 +36,7 @@ class Addon {
     checkFollowUps: typeof checkFollowUps;
     runSpringerProbe?: typeof runSpringerProbe;
     runSpringerDiscoveryCheck?: typeof runSpringerDiscoveryCheck;
+    openSpringerLogin?: typeof openSpringerLogin;
   };
 
   constructor() {
@@ -56,6 +60,7 @@ class Addon {
     if (this.data.env === "development") {
       this.api.runSpringerProbe = runSpringerProbe;
       this.api.runSpringerDiscoveryCheck = runSpringerDiscoveryCheck;
+      this.api.openSpringerLogin = openSpringerLogin;
     }
   }
 }
